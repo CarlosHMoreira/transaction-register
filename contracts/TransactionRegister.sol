e298430fc769e97a83e704131d6b94476f1c4bf5
Carlos Henrique Moreira <kmoreira620@gmail.com>
2019-06-12 23:37:45 -0300

    feat(config-enviroment):  Creates contracts

--- contracts/TransactionRegister.sol
+++ contracts/TransactionRegister.sol
@@ -10,7 +10,7 @@ contract TransactionRegister {
         uint accountId;
         uint accountPlanId;
     }
-    
+
     event TransactionCreated(
         uint id,
         string description,
@@ -20,31 +20,31 @@ contract TransactionRegister {
         uint accountId,
         uint accountPlanId
     );
-    
+
     address public owner;
     mapping(uint => Transaction) public transactions;
     uint public idSequence;
-    
+
     constructor() public {
         owner = msg.sender;
         idSequence = 0;
     }
-    
+
     function incrementId() private {
         idSequence++;
     }
 
     function createTransaction(
-        string memory _description, 
-        string memory _document, 
-        uint256 _value, 
-        uint _accountId, 
+        string memory _description,
+        string memory _document,
+        uint256 _value,
+        uint _accountId,
         uint _accountPlanId
     ) public returns (uint)
     {
         incrementId();
         uint actualTimestamp = block.timestamp;
-        
+
         Transaction memory newTransaction = Transaction({
             id: idSequence,
             description: _description,
@@ -54,19 +54,19 @@ contract TransactionRegister {
             accountId: _accountId,
             accountPlanId: _accountPlanId
         });
-        
+
         emit TransactionCreated(
-            idSequence, 
-            _description, 
-            _document, 
-            _value, 
-            actualTimestamp, 
-            _accountId, 
+            idSequence,
+            _description,
+            _document,
+            _value,
+            actualTimestamp,
+            _accountId,
             _accountPlanId
         );
-        
+
         transactions[idSequence] = newTransaction;
-        
+
         return idSequence;
     }
 }
